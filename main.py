ab20fa7105a8c04c9d628572a6bfeb8bd18f59ac
Bandoh <kelvinbandoh@hotmail.com>
2025-12-16 13:26:51 +0000

    included memory manager class call

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # from app.personal_agent import Agent
-from app.vector_db import get_vector_store
+from app.utils.vector_db import get_vector_store
 from app.personal_assistant import Agent
+from app.utils.memory_management import MemoryManager
 
 
 def main():
     get_vector_store()
     prompt = input("Me: ")
-    agent = Agent()
+    memory = MemoryManager()
+    agent = Agent(memory)
     while True:
 
         resp = agent.run(prompt)
